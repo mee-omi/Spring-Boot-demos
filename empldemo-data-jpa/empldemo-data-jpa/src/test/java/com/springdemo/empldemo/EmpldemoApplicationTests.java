9f2a6922010f0a1bf92d351e2afe378eed6d4dab
Omkar A <omkara.adg@gmail.com>
2023-05-12 20:22:01 +0530

    Initial Commit

---
+++ empldemo-data-jpa/empldemo-data-jpa/src/test/java/com/springdemo/empldemo/EmpldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springdemo.empldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

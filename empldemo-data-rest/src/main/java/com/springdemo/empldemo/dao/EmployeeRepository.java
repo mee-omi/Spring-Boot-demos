5b32e8a37945d8111800bf7712a465d0e6f70ac7
Omkar A <omkara.adg@gmail.com>
2023-05-12 20:28:52 +0530

    Intial commit

---
+++ empldemo-data-rest/src/main/java/com/springdemo/empldemo/dao/EmployeeRepository.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.springdemo.empldemo.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import com.springdemo.empldemo.entity.Employee;
+
+/**
+ * @author Omkar
+ * creating repository for employee class
+ * annotation repository rest resource used to specify custom path
+ */
+//@RepositoryRestResource(path="members")
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+
+}
---
+++ empldemo-data-rest/src/main/java/com/springdemo/empldemo/dao/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author Omkar
+ *
+ */
+package com.springdemo.empldemo.dao;
